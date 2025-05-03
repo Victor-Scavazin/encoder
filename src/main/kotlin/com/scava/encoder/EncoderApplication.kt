2181b955ed9c0e91aac0ec7a021746691b405e11
Scavazin <scavadev@gmail.com>
2025-05-03 15:30:42 -0300

    first commit

---
+++ src/main/kotlin/com/scava/encoder/EncoderApplication.kt
@@ -0,0 +1,11 @@
+package com.scava.encoder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EncoderApplication
+
+fun main(args: Array<String>) {
+	runApplication<EncoderApplication>(*args)
+}
---
+++ src/test/kotlin/com/scava/encoder/EncoderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scava.encoder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EncoderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
